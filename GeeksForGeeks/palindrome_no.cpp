2c81f14f9adc455c02cc3be040d2b716c09ba2cc
Sid <siddharthkumar032@gmail.com>
2022-11-07 20:42:26 +0530

    Added git ignore file and Palindrome code

--- GeeksForGeeks/palindrome_no.cpp
+++ GeeksForGeeks/palindrome_no.cpp
@@ -1,7 +1,38 @@
 #include<iostream>
 using namespace std;
 
+bool checkPalindrome(int a){
+    int x = a;
+    if(x>=0 && x<10){
+        return true;
+    }
+    int rev=0 , last;
+    while(x!=0){
+        last = x%10;
+        x = x/10;
+        rev = rev*10 + last;
+    }
+        cout<<rev<<endl;
+    if(a==rev){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
 int main()
 {
+    int x, n;
+    cout<<"Enter a number : ";
+    cin>>x;
+    n=checkPalindrome(x);
+    if(n==1){
+        cout<<"The input number is a Palindrome";
+    }
+    else{
+        cout<<"The input number is not a Palindrome";
+    }
     return 0;
 }
